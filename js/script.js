6b4240fe4e480ddd38973f214a14076d060b8f50
TK <149270184+baby-got-stack@users.noreply.github.com>
2023-12-03 12:04:29 -0500

    Add light/dark mode functionality and begin page layout

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const themeIcon = document.querySelector('#theme__icon');
+const body = document.querySelector('body');
+
+themeIcon.addEventListener('click', () => {
+    body.style.transition = '0.7s';
+    body.classList.toggle('body__dark__theme');
+    themeIcon.classList.toggle('uil-moon');
+    themeIcon.classList.toggle('uil-sun');
+
+    // Save it to local storage
+    localStorage.setItem('theme', body.className);
+    localStorage.setItem('icon', themeIcon.className);
+});
+
+// Renders current theme when page is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the saved state from localStorage
+    const savedTheme = localStorage.getItem('theme');
+    const savedThemeIcon = localStorage.getItem('icon');
+
+    // If there is a saved state, apply it to the page
+    if (savedTheme) {
+        body.className = savedTheme;
+    }
+    if (savedThemeIcon) {
+        themeIcon.className = savedThemeIcon;
+    }
+
+    body.style.visibility = 'visible';
+});
